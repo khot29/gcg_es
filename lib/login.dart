d81c4efe569c3722bbe6e6484037c57f5e5fbad4
OWAIS <owaispatel75@gmail.com>
2022-01-16 18:29:50 +0530

    third day

--- lib/login.dart
+++ lib/login.dart
@@ -12,82 +12,84 @@ class login extends StatelessWidget {
       child: Container(
         alignment: Alignment.center,
         decoration: BoxDecoration(color: Color(0xFFF9F9F9)),
-        child: Column(
-          children: [
-            SizedBox(
-              height: 50.0,
-            ),
-            Text(
-              "Login",
-              style: TextStyle(
-                  fontSize: 26,
-                  fontWeight: FontWeight.bold,
-                  color: Color(0xFF000000)),
-            ),
-            SizedBox(height: 30.0),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 24.0),
-              child: TextFormField(
-                decoration: InputDecoration(
-                    enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Color(0xffFFD333))),
-                    focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Color(0xffFFD333))),
-                    focusColor: Color(0xFF000000),
-                    prefixIconColor: Color(0xFF000000),
-                    suffixIconColor: Color(0xFF000000),
-                    labelStyle: TextStyle(color: Color(0xCC000000)),
-                    fillColor: Color(0xFF000000),
-                    hoverColor: Color(0xFF000000),
-                    hintText: "Enter your Email Id",
-                    labelText: "Email Address"),
+        child: Center(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 250.0,
               ),
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 24.0),
-              child: TextFormField(
-                obscureText: true,
-                decoration: InputDecoration(
-                    enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Color(0xffFFD333))),
-                    focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Color(0xffFFD333))),
-                    prefixIconColor: Color(0xFF000000),
-                    suffixIconColor: Color(0xFF000000),
-                    focusColor: Color(0xFF000000),
-                    labelStyle: TextStyle(color: Color(0xCC000000)),
-                    fillColor: Color(0xFF000000),
-                    hoverColor: Color(0xFF000000),
-                    hintText: "Enter your Password",
-                    labelText: "Password"),
+              Text(
+                "Login",
+                style: TextStyle(
+                    fontSize: 26,
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xFF000000)),
               ),
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 24.0),
-              child: Text(
-                "Forgotten Password",
-                style: (TextStyle(color: Colors.cyan)
+              SizedBox(height: 30.0),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 24.0),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Color(0xffFFD333))),
+                      focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Color(0xffFFD333))),
+                      focusColor: Color(0xFF000000),
+                      prefixIconColor: Color(0xFF000000),
+                      suffixIconColor: Color(0xFF000000),
+                      labelStyle: TextStyle(color: Color(0xCC000000)),
+                      fillColor: Color(0xFF000000),
+                      hoverColor: Color(0xFF000000),
+                      hintText: "Enter your Email Id",
+                      labelText: "Email Address"),
                 ),
               ),
-            ),
-            //remember me feature to add STARTS
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 24.0),
+                child: TextFormField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Color(0xffFFD333))),
+                      focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Color(0xffFFD333))),
+                      prefixIconColor: Color(0xFF000000),
+                      suffixIconColor: Color(0xFF000000),
+                      focusColor: Color(0xFF000000),
+                      labelStyle: TextStyle(color: Color(0xCC000000)),
+                      fillColor: Color(0xFF000000),
+                      hoverColor: Color(0xFF000000),
+                      hintText: "Enter your Password",
+                      labelText: "Password"),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 24.0),
+                child: Text(
+                  "Forgotten Password",
+                  style: (TextStyle(color: Colors.cyan)
+                  ),
+                ),
+              ),
+              //remember me feature to add STARTS
 
-            //rememberme feature ENDS
-            Padding(
-              padding: EdgeInsets.all(16.0),
-              child: ElevatedButton(
-                  style:
-                      TextButton.styleFrom(backgroundColor: Color(0xD9FFD333)),
-                  onPressed: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => homepage()));
-                  }, 
-                  child: Text(
-                    "Login",
-                    textAlign: TextAlign.center,
-                  )),
-            )
-          ],
+              //rememberme feature ENDS
+              Padding(
+                padding: EdgeInsets.all(16.0),
+                child: ElevatedButton(
+                    style:
+                        TextButton.styleFrom(backgroundColor: Color(0xD9FFD333)),
+                    onPressed: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => homepage()));
+                    }, 
+                    child: Text(
+                      "Login",
+                      textAlign: TextAlign.center,
+                    )),
+              )
+            ],
+          ),
         ),
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:ui';
 
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 // import 'package:hexcolor/hexcolor.dart';
 import 'package:flutter/cupertino.dart';
@@ -8,6 +9,9 @@ import 'package:flutter_test/flutter_test.dart';
 import 'login.dart';
 import 'register.dart';
 import 'splash.dart';
+// import 'package:carousel_pro/carousel_pro.dart';
+// import 'package:carousel_nullsafety/carousel_nullsafety.dart';
+// import 'package:carousel_pro_nullsafety/carousel_pro_nullsafety.dart';
 // import 'package:iqra_technology/pages/login.dart';
 // import 'package:iqra_technology/pages/register.dart';
 
@@ -22,9 +26,14 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -41,9 +50,14 @@ class MyApp extends StatelessWidget {
 
 //Home Page//
 
-class homepage extends StatelessWidget {
+class homepage extends StatefulWidget {
   const homepage({Key? key}) : super(key: key);
 
+  @override
+  State<homepage> createState() => _homepageState();
+}
+
+class _homepageState extends State<homepage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -55,7 +69,7 @@ class homepage extends StatelessWidget {
             Image.asset(
               'assets/images/logo.png',
               fit: BoxFit.contain,
-              height: 32,
+              height: 26,
             ),
           ],
         ),
@@ -72,8 +86,9 @@ class homepage extends StatelessWidget {
             // highlightColor: Color(0xFFFFD333),
           ),
           IconButton(
-            onPressed:() {
-              Navigator.push(context,MaterialPageRoute(builder: (context) => homepage()));
+            onPressed: () {
+              Navigator.push(
+                  context, MaterialPageRoute(builder: (context) => homepage()));
             },
             icon: Icon(Icons.favorite_outline),
             // focusColor: Color(0xFFFFD333),
@@ -94,19 +109,245 @@ class homepage extends StatelessWidget {
           ),
         ],
       ),
-      body: Container(
-        
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            SizedBox(
+              height: 200,
+              child: ListView(
+                children: [
+                  CarouselSlider(
+                    items: [
+                      //1st photo
+                      Container(
+                        margin: EdgeInsets.all(8.0),
+                        decoration: BoxDecoration(
+                          // borderRadius: BorderRadius.circular(8.0),
+                          image: DecorationImage(
+                            image: AssetImage(
+                                "assets/images/slider/GCG_ES_Slider-1.jpg"),
+                            fit: BoxFit.fill,
+                          ),
+                        ),
+                      ),
+                      //2nd photo
+                      Container(
+                        margin: EdgeInsets.all(8.0),
+                        decoration: BoxDecoration(
+                          // borderRadius: BorderRadius.circular(8.0),
+                          image: DecorationImage(
+                            image: AssetImage(
+                                "assets/images/slider/GCG_ES_Slider-4-crop.jpg"),
+                            fit: BoxFit.fill,
+                          ),
+                        ),
+                      ),
+
+                      //3rd photo
+                      Container(
+                        margin: EdgeInsets.all(8.0),
+                        decoration: BoxDecoration(
+                          // borderRadius: BorderRadius.circular(8.0),
+                          image: DecorationImage(
+                            image: AssetImage(
+                                "assets/images/slider/GCG_ES_Slider-6.jpg"),
+                            fit: BoxFit.fill,
+                          ),
+                        ),
+                      ),
+                    ],
+                    options: CarouselOptions(
+                      height: 180.0,
+                      enlargeCenterPage: true,
+                      autoPlay: true,
+                      aspectRatio: 16 / 9,
+                      autoPlayCurve: Curves.easeOut,
+                      enableInfiniteScroll: true,
+                      autoPlayAnimationDuration: Duration(milliseconds: 800),
+                      viewportFraction: 1.0,
+                    ),
+                  )
+                ],
+              ),
+            ),
+            // 4 banners
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Container(
+                  height: 100,
+                  color: Colors.white60,
+                  child: Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: Column(
+                      children: [
+                        Icon(Icons.local_shipping_outlined),
+                        Column(
+                          children: [
+                            Text("Free Shipping"),
+                            Text("For orders from 50"),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Container(
+                  height: 100,
+                  color: Colors.white60,
+                  child: Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: Column(
+                      children: [
+                        Icon(Icons.call_sharp),
+                        Column(
+                          children: [
+                            Text("Support 24/7"),
+                            Text("Call us anytime"),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Container(
+                  height: 100,
+                  color: Colors.white60,
+                  child: Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: Column(
+                      children: [
+                        Icon(Icons.payment_sharp),
+                        Column(
+                          children: [
+                            Text("100% Safety"),
+                            Text("Only secure Payments"),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Container(
+                  height: 100,
+                  color: Colors.white60,
+                  child: Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: Column(
+                      children: [
+                        Icon(Icons.local_offer_sharp),
+                        Column(
+                          children: [
+                            Text("Hot Offers"),
+                            Text("Disounts upto 90%"),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            //featured products
+            Container(
+              color: Colors.redAccent,
+              child: Column(
+                children: [
+                  Row(
+                    
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text("Featured Products"),
+                      Text("View all"),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          height: 200,
+                          width: MediaQuery.of(context).size.width - 20,
+                          child: Text("Products to be display"),
+                          decoration: BoxDecoration(
+                              border: Border.all(
+                                  color: Color(0xFFFFD333),
+                                  width: 2.0,
+                                  style: BorderStyle.solid)),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            //ad : industry soluions
+            Container(
+              child:
+                  Image(image: AssetImage("assets/images/banner/banner.jpg")),
+            ),
+            //Bestsellers
+            Container(
+              color: Colors.redAccent,
+              child: Column(
+                children: [
+                  Row(
+                    
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                    Text("Featured Products"),
+                    Text("View all"),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          height: 200,
+                          width: MediaQuery.of(context).size.width - 20,
+                          child: Text("Products to be display"),
+                          decoration: BoxDecoration(
+                              border: Border.all(
+                                  color: Color(0xFFFFD333),
+                                  width: 2.0,
+                                  style: BorderStyle.solid)),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            //new arrivals
+            //company names in a row
+
+            //toprated &
+            //& special offers
+          ],
+        ),
       ),
+
+      //section 1 starts
+
+      //section 1 ends
+
       drawer: Drawer(
         child: ListView(
-          padding: EdgeInsets.zero,
+          padding: EdgeInsets.fromLTRB(0.0, 30.0, 0.0, 0.0),
           children: [
             ListTile(
-              title: Text("Menu",style: TextStyle(fontWeight: FontWeight.bold),),
+              title: Text(
+                "Menu",
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
               hoverColor: Color(0xFFFFFFFF),
               onTap: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => homepage()));
+                // Navigator.push(context,
+                //     MaterialPageRoute(builder: (context) => homepage()));
               },
             ),
             ListTile(
@@ -124,12 +365,114 @@ class homepage extends StatelessWidget {
               },
             ),
             ListTile(
-              title: Text("All Categories"),      
+              title: Text("All Categories"),
               onTap: () {
                 Navigator.push(context,
                     MaterialPageRoute(builder: (context) => homepage()));
               },
             ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(30.0, 0.0, 0.0, 0.0),
+              child: Column(
+                children: [
+                  ListTile(
+                    title: Text("Printer"),
+                    onTap: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => homepage()));
+                    },
+                  ),
+                  ListTile(
+                    title: Text("Toners"),
+                    onTap: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => homepage()));
+                    },
+                  ),
+                  ListTile(
+                    title: Text("Service"),
+                    onTap: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => homepage()));
+                    },
+                  ),
+                ],
+              ),
+            ),
+            ListTile(
+              title: Text("My Account"),
+              onTap: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => homepage()));
+              },
+            ),
+            //myaccount details starts
+
+            Padding(
+              padding: const EdgeInsets.fromLTRB(30.0, 0.0, 0.0, 0.0),
+              child: Column(
+                children: [
+                  ListTile(
+                    title: Text("Dashboard"),
+                    onTap: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => homepage()));
+                    },
+                  ),
+                  ListTile(
+                    title: Text("Edit Profile"),
+                    onTap: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => homepage()));
+                    },
+                  ),
+                  ListTile(
+                    title: Text("Order History"),
+                    onTap: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => homepage()));
+                    },
+                  ),
+                  ListTile(
+                    title: Text("Invoice History"),
+                    onTap: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => homepage()));
+                    },
+                  ),
+                  ListTile(
+                    title: Text("Shipment History"),
+                    onTap: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => homepage()));
+                    },
+                  ),
+                  ListTile(
+                    title: Text("Address Book"),
+                    onTap: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => homepage()));
+                    },
+                  ),
+                  ListTile(
+                    title: Text("Change Password"),
+                    onTap: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => homepage()));
+                    },
+                  ),
+                  ListTile(
+                    title: Text("Logout"),
+                    onTap: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => homepage()));
+                    },
+                  ),
+                ],
+              ),
+            ),
+
+            //myaccount details ends
             ListTile(
               title: Text("Login"),
               onTap: () {
